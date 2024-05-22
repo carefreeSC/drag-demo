8a1dd7547954486e706fc33c4eb06d1e787b46b9
zhangshaocong <CarefreeSC@outlook.com>
2024-05-22 11:35:08 +0800

    fork

---
+++ src/custom-component/VChart/Component.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-chart 
+        ref="chart" 
+        class="chart" 
+        :option="propValue.option"
+        autoresize
+    />
+</template>
+
+<script>
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import { ScatterChart, BarChart, LineChart } from 'echarts/charts'
+import OnEvent from '../common/OnEvent'
+import { TitleComponent, TooltipComponent, LegendComponent, GridComponent } from 'echarts/components'
+import VChart from 'vue-echarts'
+
+use([
+    CanvasRenderer,
+    ScatterChart,
+    TitleComponent,
+    TooltipComponent,
+    LegendComponent,
+    GridComponent,
+    BarChart,
+    LineChart,
+])
+
+export default {
+    components: {
+        VChart,
+    },
+    extends: OnEvent,
+    props: {
+        propValue: {
+            type: Object,
+            require: true,
+            default: () => {},
+        },
+        element: {
+            type: Object,
+            default: () => {},
+        },
+    },
+}
+</script>
+
+<style scoped>
+.chart {
+    height: 400px;
+}
+</style>
